2332e9acdd3e42009d5c8e2b63bf6b51daf225bb
Steven Fisher <steven.fisher22@gmail.com>
2019-03-22 17:15:24 -0500

    simple input rendered

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function App() {
-    return (
-        <div>
-            Test
-        </div>
-    )
+class Example extends React.Component {
+    state = { text: ''};
+
+    handleChange = (event) => {
+        this.setState({
+            text: event.target.value
+        });
+    };
+
+    render() {
+        return (
+            <input 
+                type="text"
+                value={this.state.text}
+                onChange={this.handleChange}
+            />
+        )
+    }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Example />, document.getElementById('root'))
